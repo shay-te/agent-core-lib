9a178b7ccb7754b9d5d32c095b166e2f3c9a17e2
shay.te@gmail.com <shay.te@gmail.com>
2026-05-31 20:32:26 +0300

    Update UNA-2719

--- agent_core_lib/data_layers/data/db/entities/agent.py
+++
@@ -1,9 +0,0 @@
-# agent_import
-
-from core_lib.data_layers.data.db.sqlalchemy.base import Base
-
-
-class Agent(Base):
-    __tablename__ = 'agent'
-
-    # agent_column
--- agent_core_lib/data_layers/data_access/agent_data_access.py
+++
@@ -1,9 +0,0 @@
-from core_lib.data_layers.data_access.data_access import DataAccess
-# agent_connections_imports
-# agent_function_imports
-# agent_entity_imports
-
-
-class Agent(DataAccess):
-# agent_init
-# agent_functions
